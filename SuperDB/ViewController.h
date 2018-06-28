bb2bb323b9e12d1706e34a7a4e7a6ee928796b82
Devepre <bicycle2life@gmail.com>
2018-06-28 19:36:28 +0300

    Initial Commit

---
+++ SuperDB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuperDB
+//
+//  Created by Limitation on 6/28/18.
+//  Copyright © 2018 Limitation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SuperDB/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SuperDB
+//
+//  Created by Limitation on 6/28/18.
+//  Copyright © 2018 Limitation. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
